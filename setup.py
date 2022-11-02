b3dc862c308296e958ec046b47403734d13b1693
Elephant Liu <lexdene@gmail.com>
2022-11-02 17:05:03 +0800

    add client

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='aio-prometheus-client',
+    version='0.0.1',
+    packages=find_packages(include=['aio_prometheus_client']),
+    python_requires='>3.10.0',
+
+    install_requires=[
+        'httpx',
+    ],
+
+    license='LGPL',
+    url='https://github.com/lexdene/python-aio-prometheus-client',
+    description='Prometheus Http Client for Asyncio',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+)
