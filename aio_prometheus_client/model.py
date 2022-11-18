0d336acd96e20a20c3d1db78935375a3e2f238fb
Elephant Liu <lexdene@users.noreply.github.com>
2022-11-18 11:49:16 +0800

    support query range (#7)

--- aio_prometheus_client/model.py
+++ aio_prometheus_client/model.py
@@ -42,6 +42,36 @@ class InstantVector:
         )
 
 
+@dataclass
+class RangeSeries:
+    metric: dict
+    values: List[Scalar]
+
+    @classmethod
+    def from_data(cls, data):
+        return cls(
+            metric=data['metric'],
+            values=[
+                Scalar.from_data(i)
+                for i in data['values']
+            ]
+        )
+
+
+@dataclass
+class RangeVector:
+    series: List[RangeSeries]
+
+    @classmethod
+    def from_data(cls, data):
+        return cls(
+            series=[
+                RangeSeries.from_data(i)
+                for i in data
+            ]
+        )
+
+
 def parse_data(data):
     result_type = data['resultType']
     result = data['result']
@@ -50,3 +80,7 @@ def parse_data(data):
         return Scalar.from_data(result)
     elif result_type == 'vector':
         return InstantVector.from_data(result)
+    elif result_type == 'matrix':
+        return RangeVector.from_data(result)
+
+    raise ValueError('no such type: %s' % result_type)
