6ce884b840029bd9733a56c99c77a308b251afcb
Elephant Liu <lexdene@users.noreply.github.com>
2022-11-18 11:16:03 +0800

    chore (#6)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='aio-prometheus-client',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(include=['aio_prometheus_client']),
     python_requires='>3.10.0',
 
